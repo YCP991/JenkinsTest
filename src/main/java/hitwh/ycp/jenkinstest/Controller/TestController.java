6577f76f9f705ff0b4c646e102b624a9a8d365b3
ChampionYoung <110388154+YCP991@users.noreply.github.com>
2024-06-12 20:11:19 +0800

    Update TestController.java

--- src/main/java/hitwh/ycp/jenkinstest/Controller/TestController.java
+++ src/main/java/hitwh/ycp/jenkinstest/Controller/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @RequestMapping("test")
+    @RequestMapping("/JenkinsTest")
     String Test(){
         return "test1";
     }
