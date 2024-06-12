fde37b503205f5435d51d4aadba6d116946cb084
ChampionYoung <110388154+YCP991@users.noreply.github.com>
2024-06-12 21:12:44 +0800

    Update JenkinsTestApplication.java

--- src/main/java/hitwh/ycp/jenkinstest/JenkinsTestApplication.java
+++ src/main/java/hitwh/ycp/jenkinstest/JenkinsTestApplication.java
@@ -2,8 +2,9 @@ package hitwh.ycp.jenkinstest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class JenkinsTestApplication {
 
     public static void main(String[] args) {
